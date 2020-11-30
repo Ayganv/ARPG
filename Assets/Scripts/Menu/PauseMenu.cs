772ae1b9fe5532a03d847627b0d1945f72d69d42
Linus Vallinder <divinator.dev@gmail.com>
2020-11-30 16:24:01 +0100

    Simple Pause Menu
    When you press Esc a simple UI shows up with an exit and a resume button.

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Menu Settings")]
+    public KeyCode MenuKey;
+
+    [Space]
+    public GameObject MenuObject;
+
+    private void Update(){
+        
+        if(Input.GetKeyDown(MenuKey)){
+
+            ToggleMenu(true);
+        }
+    }
+
+    public void ToggleMenu(bool toggleBool){
+
+        MenuObject.SetActive(toggleBool);
+    }
+
+    public void ExitButton(){
+
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+}
