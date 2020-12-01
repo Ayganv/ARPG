4b7ab3b45de7e3003da83ada0ad6deb1e143c725
Kalle Jenefors <kallejenefors@gmail.com>
2020-12-01 20:02:37 +0100

    Added Detection script with Editor visuals
    -Adds Threatscript and threatcircle which display the current range of an enemy in the editor
    
    -made folder for enemy related scripts
    -made folder for editor scripts
    -added WIP script for tracker enemy

---
+++ Assets/Editor/ThreatCircle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (ThreatScript))]
+public class ThreatCircle : Editor {
+
+    void OnSceneGUI() {
+        ThreatScript fow = (ThreatScript)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc (fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+        
+        
+    }
+
+}
