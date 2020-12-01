cefe1827a3c156aa40fb0a0bea2bce38f3c1b39d
Linus Vallinder <divinator.dev@gmail.com>
2020-12-01 13:40:43 +0100

    Created Player Prefab & Refactored Obstacle Controller Code

--- Assets/Scripts/PlayerObstacleController.cs
+++ Assets/Scripts/PlayerObstacleController.cs
@@ -2,14 +2,14 @@
 using UnityEngine.AI;
 
 public class PlayerObstacleController : MonoBehaviour{
-    public Camera cam;
-    public NavMeshAgent agent;
     
-    // Update is called once per frame
-    void Update(){
-        
+    private NavMeshAgent agent => GetComponent<NavMeshAgent>();
+    
+    public void Update(){
+
         if (Input.GetMouseButtonDown(0)){
-            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit)){
