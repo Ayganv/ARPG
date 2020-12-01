cdfea73bff0bec3ea629a8e73c2e8652344bee0f
Linus Vallinder <divinator.dev@gmail.com>
2020-12-01 22:37:17 +0100

    Player Can Interact with portals to load new scene
    interaction is eventbased and can be used for other things. Will need modification later.

---
+++ Assets/Scripts/Interactable/Portal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class Portal : MonoBehaviour
+{
+    [Header("Portal Settings")]
+    public int SceneIndex;
+
+    public void LoadScene(){
+
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{   
+    [Header("Player Interaction Key")]
+    public KeyCode InteractionKey;
+
+    [Header("Interaction Settings")]
+    public float InteractionDistance;
+
+    private GameObject ClosestInteractableObject; 
+
+    private bool CanInteract => ClosestInteractableObject != null && Vector3.Distance(transform.position, ClosestInteractableObject.transform.position) <= InteractionDistance;
+
+    private void Update() {
+
+        UpdateClosestInteractableobject();
+
+        if(Input.GetKeyDown(InteractionKey) && CanInteract){
+
+            Debug.Log($"You have interacted with {ClosestInteractableObject.name}");
+            ClosestInteractableObject.GetComponent<Interactable>().Interact();
+        }
+    }
+
+    public void UpdateClosestInteractableobject(){
+
+        foreach (var interactable in FindObjectsOfType<Interactable>())
+        {
+            if(Vector3.Distance(transform.position, interactable.transform.position) <= InteractionDistance){
+
+                if(ClosestInteractableObject == null){
+
+                    ClosestInteractableObject = interactable.gameObject;
+                    break;
+                }else if(Vector3.Distance(transform.position, interactable.transform.position) < Vector3.Distance(transform.position, ClosestInteractableObject.transform.position)){
+
+                    ClosestInteractableObject = interactable.gameObject;
+                    break;
+                }
+            }
+        }
+    }
+}
