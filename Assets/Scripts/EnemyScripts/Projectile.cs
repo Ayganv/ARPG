e3c67bffb44337cfab3573616f6c5bfa2e2808e7
jimmiels <jimmie.lundin@student.forsbergsskola.se>
2020-12-02 09:35:24 +0100

    Adds projectile changes so it destroys on hit

--- Assets/Scripts/EnemyScripts/Projectile.cs
+++ Assets/Scripts/EnemyScripts/Projectile.cs
@@ -29,7 +29,7 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") || other.CompareTag("Wall"))
         {
             DestroyProjectile();
         }
