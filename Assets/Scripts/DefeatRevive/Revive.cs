3db29779bff723c66076757200ce13ace530240b
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-12-03 13:48:47 +0100

    added health script and text and refactoring

---
+++ Assets/Scripts/DefeatRevive/Revive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revive : MonoBehaviour
+{
+
+    public GameObject gameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ReviveButtom()
+    {
+        GameObject.FindWithTag("Player").SetActive(true);
+        FindObjectOfType<Death>().Health = 100;
+        gameover.SetActive(false);
+    }
+
+    
+}
