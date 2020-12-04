b0545fe798d6ee5813e8d7f262416c56ee80539e
Joel Ring <joel.ring@gmail.com>
2020-12-05 00:51:18 +0100

    adds simple camera script to pathfinderDevScene

---
+++ Assets/Scripts/PathFinderDevScene/JoelsCameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace PathFinderDevScene{
+    public class JoelsCameraController : MonoBehaviour
+    {
+    
+        public GameObject targetObject;
+
+
+        private Vector3 _offset;
+        // Start is called before the first frame update
+
+        void Start(){
+
+            _offset = targetObject.transform.position - transform.position;
+        }
+
+        // Update is called once per frame
+        void Update(){
+
+            transform.position = targetObject.transform.position - _offset;
+        }
+    }
+}
+
