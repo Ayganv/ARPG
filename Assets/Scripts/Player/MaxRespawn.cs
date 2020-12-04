828e83f903db5c3b619ba583680c27576cf70c06
Max Kassander <maximilian.kassander@gmail.com>
2020-12-04 14:05:53 +0100

    Respawn put into its own script

---
+++ Assets/Scripts/Player/MaxRespawn.cs
@@ -0,0 +1,44 @@
+﻿//using System.Collections;
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MaxRespawn : MonoBehaviour
+{
+    public Vector3 CurrentSpawn;
+
+
+    private PlayerHealthWithDeath playerHealth;
+    private void Start()
+    {
+        CurrentSpawn = this.transform.position;
+        playerHealth = GetComponent<PlayerHealthWithDeath>();
+    }
+
+    private void Update()
+    {
+        //Temporary commands for testing
+        if (Input.GetKeyDown(KeyCode.L)) CurrentSpawn = this.transform.position;
+    }
+    
+    public void ReviveButton()
+    {
+        playerHealth.dead = false;
+        playerHealth.health = playerHealth.maxHealth;
+
+        transform.position = CurrentSpawn;
+        playerHealth.gameover.SetActive(false);
+        GetComponent<NavMeshAgent>().destination = CurrentSpawn;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Respawn")
+        {
+            CurrentSpawn = other.transform.position;
+            print("new checkpoint set");
+        }
+    }
+}
