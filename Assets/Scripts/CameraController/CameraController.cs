11fa455624a7cdad80273829354d0e6cbfb989ca
Linus Vallinder <divinator.dev@gmail.com>
2020-12-04 16:33:46 +0100

    Added Zoom To Camera
    Added Zoom and created new prefab

--- Assets/Scripts/CameraController/CameraController.cs
+++ Assets/Scripts/CameraController/CameraController.cs
@@ -2,7 +2,6 @@
 
 namespace CameraController
 {
-    [RequireComponent(typeof(Camera))]
     public class CameraController : MonoBehaviour
     {
         [Header("Camera Target Settings")]
@@ -10,6 +9,20 @@ namespace CameraController
 
         [Space]
         public bool TargetIsPlayer;
+
+        [Space]
+        public Transform CameraFocus;
+
+        [Header("Camera Zoom Settings")]
+        public bool CameraCanZoom = true;
+
+        [Space]
+        public float ZoomSpeed;
+
+        [Space]
+        public float ZoomMin;
+
+        public float ZoomMax;
 
         [Header("Camera Offset Settings")]
         public Vector3 PositionOffset;
@@ -35,19 +48,41 @@ namespace CameraController
 
         private void LateUpdate()
         {
+            if (CameraCanZoom)
+            {
+                UpdateZoom();
+            }
+
             UpdateCameraPosition();
 
             UpdateCameraRotation();
         }
 
+        private void UpdateZoom()
+        {
+            float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+            float distance = Vector3.Distance(Target.position, transform.position);
+
+            if (distance < ZoomMin && scrollInput > 0)
+            {
+                return;
+            }
+            else if (distance > ZoomMax && scrollInput < 0)
+            {
+                return;
+            }
+
+            transform.position += transform.forward * scrollInput * ZoomSpeed;
+        }
+
         private void UpdateCameraPosition()
         {
-            transform.position = Vector3.Lerp(transform.position, GetDesiredPosition(Target.position), CameraSpeed);
+            CameraFocus.position = Vector3.Lerp(CameraFocus.position, GetDesiredPosition(Target.position), CameraSpeed);
         }
 
         private void UpdateCameraRotation()
         {
-            transform.rotation = RotationOffset;
+            CameraFocus.rotation = RotationOffset;
         }
 
         private Vector3 GetDesiredPosition(Vector3 TargetPositon)
