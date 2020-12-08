1ce0d981899553d93c935ad825d3553ed9e1eb2d
Max Kassander <maximilian.kassander@gmail.com>
2020-12-08 13:18:58 +0100

    Acorn spawner & Acorn

---
+++ Assets/Scripts/EnemyScripts/PatrollingEnemy/Acorn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acorn : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+
+    private PlayerHealth playerHealth;
+
+    private void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerHealth.TakeDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/PatrollingEnemy/AcornSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcornSpawner : MonoBehaviour
+{
+    public float timeBetweenShots;
+    private float timer;
+
+    public GameObject acorn;
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        if (timer >= timeBetweenShots)
+        {
+            Instantiate(acorn, transform.position, Quaternion.identity);
+            timer = 0;
+        }
+    }
+}
