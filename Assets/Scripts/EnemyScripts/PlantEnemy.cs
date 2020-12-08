aa50e4a3242b2d17d0e665c99f64bd232e2c2d71
Kalle Jenefors <kallejenefors@gmail.com>
2020-12-08 16:43:42 +0100

    Added Plant script
    also added editor to show damage radius

---
+++ Assets/Scripts/EnemyScripts/PlantEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantEnemy : MonoBehaviour
+{
+    public int damage;
+    public float timeBetweenAttack;
+    public float damageRadius;
+    private PlayerHealth _playerHealth;
+    private Transform _player;
+    private float _timeSinceLastAttack;
+
+    private void Start()
+    {
+        _player= GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        DealDamage();
+    }
+
+    void DealDamage()
+    {
+        if (Vector3.Distance(transform.position, _player.position) <= damageRadius)
+        {
+            if (_timeSinceLastAttack <= 0)
+            {
+                _playerHealth.TakeDamage(damage);
+                _timeSinceLastAttack = timeBetweenAttack;
+            }
+            else
+            {
+                _timeSinceLastAttack -= Time.deltaTime;
+            }
+        }
+    }
+}
