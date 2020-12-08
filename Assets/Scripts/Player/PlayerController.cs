0c3934356400b23692bbfa555729000f42fce74e
Linus Vallinder <divinator.dev@gmail.com>
2020-12-08 14:30:11 +0100

    Renamed variables & changed player prefab

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class PlayerController : MonoBehaviour{
-
+public class PlayerController : MonoBehaviour
+{
     private NavMeshAgent agent => GetComponent<NavMeshAgent>();
-    
-    public void Update(){
 
-        if (Input.GetMouseButtonDown(0)){
-            
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out RaycastHit hit))
