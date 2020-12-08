6fe99197d86d53a75f3dc6cad2ca76a5b49f3c8e
Linus Vallinder <divinator.dev@gmail.com>
2020-12-08 13:48:12 +0100

    Created Player Manager singleton

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        public static PlayerManager Instance;
+
+        public PlayerHealth playerHealth => GetComponent<PlayerHealth>();
+
+        public PlayerInteract playerInteract => GetComponent<PlayerInteract>();
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+    }
+}
