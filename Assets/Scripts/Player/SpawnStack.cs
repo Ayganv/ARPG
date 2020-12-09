8e1de40c2d397dfff915a852a08a129ebd2889e9
EmilHuzell <65963728+EmilHuzell@users.noreply.github.com>
2020-12-09 13:50:06 +0100

    Creates ceckpoint api and checkpoints scriptable object

--- Assets/Scripts/Player/SpawnStack.cs
+++ Assets/Scripts/Player/SpawnStack.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public class Spawn
+public class CheckPoint
 {
-    private Vector3 position;
+    private Vector3 SpawnPosition;
+    private Vector3 GoalPosition;
     private string message;
-    public Spawn(Vector3 position, string message)
+    public int Number;
+    private static int successRadius = 3;
+
+    
+    //public static delegate bool successCheck();
+    public bool successCheck(Vector3 Currentposition)
     {
-        this.position = position;
-        this.message = message;
+        Debug.Log(Vector3.Distance(Currentposition, GoalPosition));
+        return Vector3.Distance(Currentposition, GoalPosition) < CheckPoint.successRadius;
     }
-    public Vector3 GetLocation()
+    public CheckPoint(Vector3 SpawnPosition, Vector3 GoalPosition, string Message, int Number)
     {
-        return this.position;
+        this.SpawnPosition = SpawnPosition;
+        this.GoalPosition = GoalPosition;
+        this.message = Message;
+        this.Number = Number;
+    }
+    public Vector3 GetSpawnPosition()
+    {
+        return this.SpawnPosition;
     }
     public string GetMessage()
     {
@@ -24,34 +36,49 @@ public class Spawn
 }
 public class SpawnStack : MonoBehaviour
 {
-    
-    private Stack<Spawn> Stack = new Stack<Spawn>();
+    public List<CheckPointSO> CheckPoints;
+    private Stack<CheckPoint> Stack = new Stack<CheckPoint>();
     public GameObject SpawnTask;
-    public string objective; 
     
-
-    public void AddSpawn(Vector3 position)
+    private void Update()
     {
-        this.Stack.Push(new Spawn(position, $"attempt {this.Stack.Count}"));
+        this.UpdateCheckPoint();
+        if(Input.GetKeyDown("a"))
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(-15,0,0);
+        if(Input.GetKeyDown("s"))  
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,-15);  
+        if(Input.GetKeyDown("w"))  
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,15); 
+        if(Input.GetKeyDown("d"))  
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(15,0,0);     
+    }
+    //add SO
+    public void AddCheckPoint(CheckPointSO checkpoint)
+    {
+        this.Stack.Push(new CheckPoint(checkpoint.SpawnPosition, checkpoint.GoalPosition, checkpoint.Message, checkpoint.Number));
         this.WriteSpawnTask();
+    }
+    public void UpdateCheckPoint()
+    {
+        if(this.Stack.Count == 0)
+        {
+            this.AddCheckPoint(this.CheckPoints[0]);
+        }
+        if(this.getLatestSpawnData().successCheck(gameObject.transform.position))
+        {
+            Debug.Log(this.getLatestSpawnData().Number);
+            this.AddCheckPoint(CheckPoints[this.getLatestSpawnData().Number + 1]);
+        }
+
     }
     public void WriteSpawnTask()
     {
-        SpawnTask.GetComponent<Text>().text = this.objective + "\n" + this.getLatestSpawnData().GetMessage();
+        SpawnTask.GetComponent<Text>().text = this.getLatestSpawnData().GetMessage();
 
     }
-    public Spawn goBackToLatestSpawn(){
-        //SpawnTask.GetComponent<Text>().text = this.Stack.Peek().GetMessage();
+    
+    public CheckPoint getLatestSpawnData(){
         return this.Stack.Peek();
-    }
-    public Spawn getLatestSpawnData(){
-        return this.Stack.Peek();
-    }
-    public void NewChapter(Vector3 position, string objective){
-        this.Stack.Clear();
-        this.AddSpawn(position);  
-        this.objective = objective;
-        this.WriteSpawnTask();
     }
 
 }
