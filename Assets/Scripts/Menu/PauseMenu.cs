c7551b43205f0708c0ada2335a64fc1a0fff53f5
Linus Vallinder <divinator.dev@gmail.com>
2020-12-11 14:42:50 +0100

    Changed Camera Zoom

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -32,8 +32,6 @@ public class PauseMenu : MonoBehaviour
             instance = this;
         }
 
-        DontDestroyOnLoad(this.gameObject);
-
         if (startPlaying == null){
             startPlaying = new UnityEvent();
         }
