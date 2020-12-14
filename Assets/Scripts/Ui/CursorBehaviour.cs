6db7ca91e345d76941ecc13d7aa7ba0b87592514
Kalle Jenefors <kallejenefors@gmail.com>
2020-12-14 17:27:55 +0100

    adds Cursor script
    help

---
+++ Assets/Scripts/Ui/CursorBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class CursorBehaviour : MonoBehaviour
+{
+    public Texture2D clickableCursor;
+    public Texture2D normalCursor;
+    private PlayerController _playerController;
+    private Transform _playerTransform;
+    private float _playerDistance;
+    private void Start()
+    {
+        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    private void Update()
+    {
+        _playerDistance = Vector3.Distance(transform.position, _playerTransform.position);
+        if (_playerController.MaxDestinationDistance <= _playerDistance)
+        {
+            OnMouseOver();
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        Cursor.SetCursor(clickableCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
