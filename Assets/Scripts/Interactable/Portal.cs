a9bdf6627cc2d60cc27408e4f151727bb70e5e86
Joel Ring <joel.ring@gmail.com>
2020-12-14 22:40:29 +0100

    adds portal sound

--- Assets/Scripts/Interactable/Portal.cs
+++ Assets/Scripts/Interactable/Portal.cs
@@ -9,8 +9,16 @@ public class Portal : MonoBehaviour
     [Header("Portal Settings")]
     public int SceneIndex;
 
+    public UnityEvent Teleported;
     public void LoadScene(){
-
+        StartCoroutine(StartPorting()); 
+    }
+    
+    IEnumerator StartPorting()
+    {
+        Teleported.Invoke();
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(SceneIndex);
+        
     }
 }
