0b27a435f1bd95e39ed836ee830a0f43886126cb
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-12-14 13:34:50 +0100

    refactoring spawn script

--- Assets/Scripts/Player/Spawn.cs
+++ Assets/Scripts/Player/Spawn.cs
@@ -11,14 +11,19 @@ public class Spawn : MonoBehaviour
         PlayerPrefs.SetString("Position", JsonUtility.ToJson(this.transform))
     }*/
 
-    
+
     public void ReSpawnFixed()
     {
-       PlayerManager.Instance.PlayerController.Agent.destination = FixedSpawn;
-       this.transform.position = FixedSpawn;
-       
-       //JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("Position", " "));
+        PlayerManager.Instance.PlayerController.Agent.destination = FixedSpawn;
+        this.transform.position = FixedSpawn;
+        FindObjectOfType<Death>().Die(deadbool: false, playerMovement: true,
+            gameOver: false);
+        GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().Health = 100;
+        Time.timeScale = 1;
+
+        //JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("Position", " "));
     }
+
     public void Revive()
     {
         //player.health == 100
