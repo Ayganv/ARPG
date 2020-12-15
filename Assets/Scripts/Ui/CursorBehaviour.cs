29bee3809756e45913956ff01959730fbaeb827e
Kalle Jenefors <kallejenefors@gmail.com>
2020-12-15 13:01:12 +0100

    small change in cursor script

--- Assets/Scripts/Ui/CursorBehaviour.cs
+++ Assets/Scripts/Ui/CursorBehaviour.cs
@@ -9,21 +9,18 @@ public class CursorBehaviour : MonoBehaviour
     public Texture2D clickableCursor;
     public Texture2D normalCursor;
 
-    private void Start()
-    {
-        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
-    }
+    
 
     private void OnMouseOver()
     {
         if (PlayerManager.Instance.PlayerController.CanSetDestination(transform.position))
         { 
-            Cursor.SetCursor(clickableCursor, Vector2.zero, CursorMode.ForceSoftware);
+            Cursor.SetCursor(clickableCursor, Vector2.zero, CursorMode.Auto);
         }
     }
 
     private void OnMouseExit()
-    {
-        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.ForceSoftware);
+    {   
+        Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.Auto);
     }
 }
