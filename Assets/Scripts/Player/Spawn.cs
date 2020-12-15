01b49b3ab2765f52f9b8d93d0712ce047bb292b4
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-12-15 18:31:44 +0100

    Adding timescale for spawn.cs

--- Assets/Scripts/Player/Spawn.cs
+++ Assets/Scripts/Player/Spawn.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Player;
@@ -18,6 +19,7 @@ public class Spawn : MonoBehaviour
         this.transform.position = FixedSpawn;
         FindObjectOfType<Death>().gameOverMenu(gameOver: false);
         GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().Health = 100;
+        Time.timeScale = 1;
         Death.gameover.SetActive(false);
         Death.dead = false;
 
