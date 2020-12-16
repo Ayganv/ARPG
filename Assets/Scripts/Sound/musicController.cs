fd45f9eff4a96a09bac7d2e3197cb90f2900ad6e
Joel Ring <joel.ring@gmail.com>
2020-12-16 16:53:33 +0100

    adds new level1 music and death music event

--- Assets/Scripts/Sound/musicController.cs
+++ Assets/Scripts/Sound/musicController.cs
@@ -1,38 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class musicController : MonoBehaviour
-{
-    private FMOD.Studio.EventInstance instance;
-    
-    [FMODUnity.EventRef]
-    public string fmodEvent;
-    
-    [SerializeField] 
-    private float Init, InGame;
+public class musicController : MonoBehaviour{ 
+    public float value = 1;
+   private void Start(){
+           
+       FMODUnity.RuntimeManager.StudioSystem.setParameterByName("musicSequenceParameter", value); 
+   }
 
-    [SerializeField] 
-    private float InGame2;
-
-    [SerializeField]
-    private float musicSequenceParameter;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
-        instance.start();
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        instance.setParameterByName("Init", Init);
-        instance.setParameterByName("InGame", InGame);
-        instance.setParameterByName("InGame2", InGame2);
-        instance.setParameterByName("musicSequenceParameter", musicSequenceParameter);
-        
-    }
+   public void playLevel2(){
+       FMODUnity.RuntimeManager.StudioSystem.setParameterByName("musicSequenceParameter", 2); 
+   }
+   public void playerDeath(){
+       FMODUnity.RuntimeManager.StudioSystem.setParameterByName("musicSequenceParameter", 3); 
+   }
+   
+   public void playerDeath2(){
+       FMODUnity.RuntimeManager.StudioSystem.setParameterByName("musicSequenceParameter", 4); 
+   }
 }
