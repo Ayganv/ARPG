7e3dc237e6d389469e03efe720bf63d3ca1824cd
Max Kassander <maximilian.kassander@gmail.com>
2020-12-17 21:54:55 +0100

    Level sign fades away

---
+++ Assets/Scripts/Ui/FadeAwayImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAwayImage : MonoBehaviour
+{
+    private Image levelSign => GetComponent<Image>();
+
+    private float timer;
+    public float timeUntilFade = 5;
+    
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeUntilFade)
+        {
+            FadeOut();
+        }
+    }
+
+    public void FadeOut()
+    {
+        levelSign.CrossFadeAlpha(0f, 2f,false);
+    }
+}
---
+++ Assets/Scripts/Ui/FadeAwayText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAwayText : MonoBehaviour
+{
+    private Text text => GetComponent<Text>();
+
+    private float timer;
+    public float timeUntilFade = 5;
+    
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeUntilFade)
+        {
+            FadeOut();
+        }
+    }
+
+    public void FadeOut()
+    {
+        text.CrossFadeAlpha(0f,2f,false);
+    }
+}
